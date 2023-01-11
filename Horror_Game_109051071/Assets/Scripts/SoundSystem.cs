ba6c61d763932806c54f3fe456a3175388314e1f
陳昕儀 <113577945+lili0972311@users.noreply.github.com>
2023-01-11 15:24:40 +0800

    step11
    烘培很久，網路好像中斷了

---
+++ Horror_Game_109051071/Assets/Scripts/SoundSystem.cs
@@ -0,0 +1,33 @@
+﻿using OpenCover.Framework.Model;
+using UnityEngine;
+
+
+namespace KUMO
+{
+    /// <summary>
+    /// 音效系統
+    /// </summary>
+    /// 要求元件:在第一次套用此腳本時會添加裡面指定的元件
+    [RequireComponent(typeof(AudioSource))]
+    public class SoundSystem : MonoBehaviour
+    {
+        private AudioSource aud;
+
+        private void Awake()
+        {
+            aud = GetComponent<AudioSource>();
+        }
+
+        ///<Summary>
+        ///播放音效
+        ///</Summary>
+        ///<param name="sound">要播放的音效</param>
+        public void PlaySound(AudioClip sound)
+        {
+            //音效來源.播放一次音效(音效);
+            aud.PlayOneShot(sound);
+        }
+
+
+    }
+}
